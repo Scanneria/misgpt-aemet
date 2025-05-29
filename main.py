866c451f3c52a0abaa79f9a89781bd003842ba13
Scanneria <scanneriav172@gmail.com>
2025-05-29 22:14:11 +0200

    main.py

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ app = FastAPI()
 
 AEMET_API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzY2FubmVyaWF2MTcyQGdtYWlsLmNvbSIsImp0aSI6IjJkNTliN2FkLWQ2MjMtNGI2MC05ZTEyLTM3N2QzMzIxMjIxYiIsImlzcyI6IkFFTUVUIiwiaWF0IjoxNzQ4NTQ1Mjk3LCJ1c2VySWQiOiIyZDU5YjdhZC1kNjIzLTRiNjAtOWUxMi0zNzdkMzMyMTIyMWIiLCJyb2xlIjoiIn0.2YnJzBm6mYwfwgIjK2-fPqPnjjBUeBug2B4iC6Gy6-U"
 
+def primer_valido(lista, campo):
+    for item in lista:
+        valor = item.get(campo)
+        if valor not in [None, "", "Ip"]:
+            return valor
+    return "Sin dato"
+
 @app.get("/meteo")
 def meteo(municipio_id: str = Query(..., description="Código INE del municipio")):
     headers = {
@@ -21,9 +28,9 @@ def meteo(municipio_id: str = Query(..., description="Código INE del municipio"
     datos = requests.get(datos_url).json()
 
     pred = datos[0]['prediccion']['dia'][0]
-    viento = pred['viento'][0].get('valor', "Sin dato")
-    estado = pred['estadoCielo'][0].get('descripcion', "Sin dato")
-    precipitacion = pred['probPrecipitacion'][0].get('valor', "Sin dato")
+    viento = primer_valido(pred.get('viento', []), 'valor')
+    estado = primer_valido(pred.get('estadoCielo', []), 'descripcion')
+    precipitacion = primer_valido(pred.get('probPrecipitacion', []), 'valor')
 
     return {
         "municipio": datos[0]['nombre'],
@@ -31,4 +38,3 @@ def meteo(municipio_id: str = Query(..., description="Código INE del municipio"
         "estado_cielo": estado,
         "precipitacion_prob": precipitacion
     }
-
